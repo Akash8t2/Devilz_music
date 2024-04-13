deaecba13a5fe52c0af61862c8f8b904395e23f2
WHITEHELL097 <101397767+WHITEHELL097@users.noreply.github.com>
2024-04-13 19:45:42 +0530

    Add files via upload

---
+++ AnonXMusic/__main__.py
@@ -0,0 +1,62 @@
+import asyncio
+import importlib
+
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
+
+import config
+from AnonXMusic import LOGGER, app, userbot
+from AnonXMusic.core.call import Anony
+from AnonXMusic.misc import sudo
+from AnonXMusic.plugins import ALL_MODULES
+from AnonXMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("Assistant client variables not defined, exiting...")
+        exit()
+    await sudo()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("Devilz Music.plugins" + all_module)
+    LOGGER("Devilz Music.plugins").info("Successfully Imported Modules...")
+    await userbot.start()
+    await Anony.start()
+    try:
+        await Anony.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+    except NoActiveGroupCall:
+        LOGGER("Devilz Music").error(
+            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+        )
+        exit()
+    except:
+        pass
+    await Anony.decorators()
+    LOGGER("Devilz Music").info(
+        b"\x00@\x00w\x00h\x00i\x00t\x00e\x00h\x00e\x00l\x00l\x000\x001\x001\x00 \x00'\x00s\x00 \x00M\x00u\x00s\x00i\x00c\x00 \x00B\x00o\x00t\x00 \x00S\x00t\x00a\x00r\x00t\x00e\x00d\x00 \x00S\x00u\x00c\x00c\x00e\x00s\x00s\x00f\x00u\x00l\x00l\x00y\x00.\x00\n\x00\n\x00D\x00o\x00n\x00'\x00t\x00 \x00f\x00o\x00r\x00g\x00e\x00t\x00 \x00t\x00o\x00 \x00v\x00i\x00s\x00i\x00t\x00 \x00@\x00d\x00i\x00l\x00_\x00k\x00i\x00_\x00b\x00a\x00a\x00t\x00e\x00e\x00 \x00a\x00n\x00d\x00 \x00@\x00h\x00e\x00a\x00r\x00t\x00_\x00t\x00a\x00l\x00k\x00z"
+    )
+    await idle()
+    await app.stop()
+    await userbot.stop()
+    LOGGER("Devilz Music").info("Stopping Devilz Music Bot...")
+
+
+if __name__ == "__main__":
+    asyncio.get_event_loop().run_until_complete(init())
---
+++ AnonXMusic/utils/inlinequery.py
@@ -0,0 +1,44 @@
+from pyrogram.types import InlineQueryResultArticle, InputTextMessageContent
+
+answer = []
+
+answer.extend(
+    [
+        InlineQueryResultArticle(
+            title="Pᴀᴜsᴇ",
+            description=f"ᴩᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/0418eba88d89932d090fe.jpg",
+            input_message_content=InputTextMessageContent("/pause"),
+        ),
+        InlineQueryResultArticle(
+            title="Rᴇsᴜᴍᴇ",
+            description=f"ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴩᴀᴜsᴇᴅ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/81d7063a5f99e279c4988.jpg",
+            input_message_content=InputTextMessageContent("/resume"),
+        ),
+        InlineQueryResultArticle(
+            title="Sᴋɪᴩ",
+            description=f"sᴋɪᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ ᴀɴᴅ ᴍᴏᴠᴇs ᴛᴏ ᴛʜᴇ ɴᴇxᴛ sᴛʀᴇᴀᴍ.",
+            thumb_url="https://te.legra.ph/file/50c10819ab94d1039442e.jpg",
+            input_message_content=InputTextMessageContent("/skip"),
+        ),
+        InlineQueryResultArticle(
+            title="Eɴᴅ",
+            description="ᴇɴᴅ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ sᴛʀᴇᴀᴍ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/00c623c72c5ad38324e73.jpg",
+            input_message_content=InputTextMessageContent("/end"),
+        ),
+        InlineQueryResultArticle(
+            title="Sʜᴜғғʟᴇ",
+            description="sʜᴜғғʟᴇ ᴛʜᴇ ǫᴜᴇᴜᴇᴅ sᴏɴɢs ɪɴ ᴩʟᴀʏʟɪsᴛ.",
+            thumb_url="https://te.legra.ph/file/68aadb1f8d3a44d6fa8f3.mp4",
+            input_message_content=InputTextMessageContent("/shuffle"),
+        ),
+        InlineQueryResultArticle(
+            title="Lᴏᴏᴩ",
+            description="ʟᴏᴏᴩ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴩʟᴀʏɪɴɢ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.",
+            thumb_url="https://te.legra.ph/file/97c4209447295a7ee24c7.jpg",
+            input_message_content=InputTextMessageContent("/loop 3"),
+        ),
+    ]
+)
